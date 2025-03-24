7cba0a4446e98508052c2b2b8e2b56286c3d554d
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-03-24 09:17:50 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,44 +1,45 @@
 public class PracticeProblem {
 
-	//1
-	public static int find(int[] array, int number) {
-		int index = -1;
-		for (int i = 0; i < array.length; i++){
-			if (number < array[i]){
-				index = i;
-				i = array.length;
-			}
-		}
-		return -1;
-	}
 
-	//2
-	public static int findLast(String[] array2, String str){
-		int index = -1;
-		for (int i = 0; i < array2.length; i++){
-			if (str.equals(array2[i])){
-				index = i;
-			{
-		{
-		return index;
-	{
+    public static int find (int[] array, int number){
+        int index = -1;
+        for (int i =0; i<array.length; i++){
+            if (number == array[i]){
+                index = i;
+                i = array.length;
+            }
+        }
+        return index;
+    }
 
+    public static int findLast(String[] arr, String text){
+        int index = -1;
+        for (int i = 0; i < arr.length; i++){
+            if (text.equals(arr[i])){
+                index = i;
+            }   
+        }
+        return index;
+    }
 
-	//3
-	public static int findScond(char[] array3, char character){
-		int index = -1;
-		int count = 0;
-		for (int i = 0; i < array3.length; i++){
-			if (count == 2){
-				i = array3.length;
-			}
-			else if (character == array3[i]){
-				index = i;
-				count++;
-			}
-		}
-		return index;
-	}
+    public static int findSecond(char[] ar, char ch){
+        int index = -1;
+        int count = 0;
+        for (int i = 0; i<ar.length; i++){
+            if (count == 2){
+                i = ar.length;
+            }
+            else if (ch == ar[i]){
+                index = i;
+                count++;
+            }
+        }
+        return index;
+    }
+    
+
+}
+
 				
 		
 }
